b3374500c6969095ce1fd668c31beb3f331da8cb
Shigetorian <shigetorakuro@outlook.com>
2019-03-03 21:08:41 +0300

    low

--- star.py
+++ star.py
@@ -2,10 +2,10 @@ import random
 import time 
 import os
 u = 0
-types = ['cs','py','js']
+types = ['cs','py','js','kt','html','css','vbs','php']
 while True:
     u+=1
-    i = random.randint(0,2)
+    i = random.randint(0,7)
     r = open('files/'+str(u)+'.'+str(types[i]),mode = 'w')
     r.write(str(random.randint(13,666)))
     r.close()
